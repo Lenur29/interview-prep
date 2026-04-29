6d065286e7913c06045b4e885a4ec1f88045afcc
Lenur Azizov <85166075+Lenur29@users.noreply.github.com>
2026-04-29 19:16:40 +0300

    refactoring: migrte to module-based pattern

--- apps/web/src/app/router.tsx
+++ apps/web/src/app/router.tsx
@@ -1,42 +1,36 @@
 import { lazy } from 'react';
 import { createBrowserRouter, Navigate } from 'react-router';
 
+import AppLayout from '@/layouts/app-layout';
+import AuthLayout from '@/layouts/auth-layout';
+import { authRoutes } from '@/modules/auth/routes';
+import { dashboardRoutes } from '@/modules/dashboard/routes';
+import { notesRoutes } from '@/modules/notes/routes';
+import { questionsRoutes } from '@/modules/questions/routes';
+import { reviewRoutes } from '@/modules/review/routes';
+import { topicsRoutes } from '@/modules/topics/routes';
+import { usersRoutes } from '@/modules/users/routes';
 import { RoutePath } from '@/shared/config/router/constants';
-import AppLayout from '@/widgets/app-layout';
-import AuthLayout from '@/widgets/auth-layout';
 
-const LoginPage = lazy(() => import('@/pages/login-page'));
-const AuthCallbackPage = lazy(() => import('@/pages/auth-callback-page'));
-const DashboardPage = lazy(() => import('@/pages/dashboard-page'));
-const TopicsPage = lazy(() => import('@/pages/topics-page'));
-const TopicPage = lazy(() => import('@/pages/topic-page'));
-const QuestionPage = lazy(() => import('@/pages/question-page'));
-const ReviewPage = lazy(() => import('@/pages/review-page'));
-const NotesPage = lazy(() => import('@/pages/notes-page'));
-const AccountPage = lazy(() => import('@/pages/account-page'));
-const NotFoundPage = lazy(() => import('@/pages/not-found-page'));
+const NotFoundPage = lazy(() => import('./not-found-page'));
 
 export const router = createBrowserRouter([
   { path: RoutePath.Root, element: <Navigate to={RoutePath.Dashboard} replace /> },
 
   {
     element: <AuthLayout />,
-    children: [
-      { path: RoutePath.Login, element: <LoginPage /> },
-      { path: RoutePath.AuthCallback, element: <AuthCallbackPage /> },
-    ],
+    children: authRoutes,
   },
 
   {
     element: <AppLayout />,
     children: [
-      { path: RoutePath.Dashboard, element: <DashboardPage /> },
-      { path: RoutePath.Topics, element: <TopicsPage /> },
-      { path: RoutePath.Topic, element: <TopicPage /> },
-      { path: RoutePath.Question, element: <QuestionPage /> },
-      { path: RoutePath.Review, element: <ReviewPage /> },
-      { path: RoutePath.Notes, element: <NotesPage /> },
-      { path: RoutePath.Account, element: <AccountPage /> },
+      ...dashboardRoutes,
+      ...usersRoutes,
+      ...topicsRoutes,
+      ...questionsRoutes,
+      ...reviewRoutes,
+      ...notesRoutes,
     ],
   },
 
